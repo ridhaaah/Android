b85d735861cad09a04d87d9750c188edbfdaae99
ridhaaah <ridhaafthm@gmail.com>
2026-09-11 10:39:26 +0530

    Add files via upload

---
+++ program4/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.registrationform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText name, name1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.name);
+        name1 = findViewById(R.id.name1);
+    }
+
+    public void Click(View view) {
+
+        String n = name.getText().toString();
+        String n1 = name1.getText().toString();
+
+        // Store data in SharedPreferences
+        SharedPreferences sp = getSharedPreferences("data", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+
+        editor.putString("name", n);
+        editor.putString("name1", n1);
+        editor.apply();
+
+        // Send data to MainActivity2
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+
+        intent.putExtra("name", n);
+        intent.putExtra("name1", n1);
+
+        startActivity(intent);
+    }
+}
